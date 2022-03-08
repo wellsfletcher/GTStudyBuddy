47759db63bad54fbb23ff11b49dc649ead6ced7e
wellsfletcher <wellsfletcher@gmail.com>
2022-03-08 16:53:20 -0500

    Added initial Xcode files.

---
+++ GTStudyBuddy/GTStudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTStudyBuddyApp.swift
+//  GTStudyBuddy
+//
+//  Created by Fletcher Wells on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GTStudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
