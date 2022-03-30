06217026b39db88f34f238ab517b2ce2b02c1b1b
ajaymoturi12 <ajaymoturi0@gmail.com>
2022-03-30 00:35:45 -0400

    separated signIn and signUp Views and cleaned up aesthetic

---
+++ GTStudyBuddy/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  GTStudyBuddy
+//
+//  Created by Ajay Moturi on 3/29/22.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
