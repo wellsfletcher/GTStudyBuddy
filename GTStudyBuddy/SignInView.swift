886226f535f74df0fec205197465a71af49a6112
ajaymoturi12 <ajaymoturi0@gmail.com>
2022-03-30 00:37:00 -0400

    separated signIn and signUp Views and cleaned up aesthetic

--- GTStudyBuddy/SignInView.swift
+++ GTStudyBuddy/SignInView.swift
@@ -12,25 +12,60 @@ import FirebaseFirestore
 struct SignInView: View {
   @State var email = ""
   @State var password = ""
-  @State var confirmPassword = ""
   @State var successLogin: Bool = false
+  @State var showPass: Bool = false
+  @State var signUp: Bool = false
   
   var body: some View {
     NavigationView {
       VStack {
-        Text("Sign up to be a part of GT Study Buddy!")
+        Text("Sign in to GT Study Buddy:")
           .font(.title)
         
         VStack(alignment: .leading) {
-          TextField("Email", text: self.$email)
-          
+            TextField("Email:", text: self.$email)
+                  .padding()
+                  .overlay(RoundedRectangle(cornerRadius: 12)
+                                      .stroke(Color.secondary, lineWidth: 1)
+                                      .foregroundColor(.clear))
+
           // Use a securefield for sensitive info
           // replaces text with dots and other secure features
-          SecureField("Password", text: self.$password)
-          
-          SecureField("Confirm Password", text: self.$confirmPassword)
+            if !showPass {
+                HStack {
+                    SecureField("Password:", text: self.$password)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 12)
+                                            .stroke(Color.secondary, lineWidth: 1)
+                                            .foregroundColor(.clear))
+                
+                    Button(action: {
+                                    showPass.toggle()
+                                }, label: {
+                                    Image(systemName: showPass ? "eye.fill" : "eye.slash.fill")
+                                })
+                }
+                .padding(.leading, 0)
+                .padding(.top, 15)
+            } else {
+                HStack {
+                    TextField("Password:", text: self.$password)
+                        .padding()
+                        .overlay(RoundedRectangle(cornerRadius: 12)
+                                            .stroke(Color.secondary, lineWidth: 1)
+                                            .foregroundColor(.clear))
+                
+                    Button(action: {
+                                    showPass.toggle()
+                                }, label: {
+                                    Image(systemName: showPass ? "eye.fill" : "eye.slash.fill")
+                                })
+                }
+                .padding(.leading, 0)
+                .padding(.top, 15)
+            }
         }
-        .padding()
+        .padding(.leading, 5)
         
         Button(action: {
           logIn()
@@ -41,20 +76,23 @@ struct SignInView: View {
             .background(.blue)
             .cornerRadius(15)
         })
+        .padding()
         NavigationLink(destination: CRNSetupView(), isActive: $successLogin) {
           EmptyView()
         }
-        
-        //first task: create sign in functionality
-        Button(action: {
-          signUp()
-        }, label: {
-          Text("Sign Up")
-            .foregroundColor(.white)
-            .frame(width: 100, height: 50)
-            .background(.blue)
-            .cornerRadius(15)
-        })
+          
+          NavigationLink(destination: SignUpView(), isActive: $signUp) {
+              Button(action: {
+                  signUp = true
+              }, label: {
+                  Text("Sign Up")
+                      .frame(width: 100, height: 50)
+                      .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.gray, lineWidth: 2.5)
+                      )
+              })
+          }
         Spacer()
         
       }
@@ -64,56 +102,18 @@ struct SignInView: View {
   }
   
   func logIn() {
-    print("log in touched")
     Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
       if error == nil {
         successLogin = true
-        print("ran here")
+        print("successfully logged in")
       } else {
         print(error?.localizedDescription as Any)
         
       }
     }
   }
-  
-  func signUp() {
-    if password.count < 8 {
-      print("Password must be at least 8 characters.")
-      return
-    }
-    
-    if password != confirmPassword {
-      print("Passwords do not match.")
-      return
-    }
-    
-    
-    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-      if let error = error as NSError? {
-        switch AuthErrorCode(rawValue: error.code) {
-        case .operationNotAllowed:
-          // Error: The given sign-in provider is disabled for this Firebase project. Enable it in the Firebase console, under the sign-in method tab of the Auth section.
-          print("Not allowed")
-        case .emailAlreadyInUse:
-          // Error: The email address is already in use by another account.
-          print("Email already in use")
-        case .invalidEmail:
-          // Error: The email address is badly formatted.
-          print("Invalid Emails")
-        default:
-          print("Other error")
-        }
-      } else {
-        let db = Firestore.firestore()
-        let user = authResult!.user
-        db.collection("users").document(user.uid).setData(["email": user.email], merge: true)
-      }
-    }
-  }
-  
-  
 }
-
+    
 struct SignInView_Previews: PreviewProvider {
   static var previews: some View {
     SignInView()
