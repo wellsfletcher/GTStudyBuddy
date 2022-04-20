8240b19ac8e88e94f4b4de347d8c03bdf9ae72b9
wellsfletcher <wellsfletcher@gmail.com>
2022-04-20 02:19:53 -0400

    #79 - typography improvements

--- GTStudyBuddy/SignInView.swift
+++ GTStudyBuddy/SignInView.swift
@@ -10,104 +10,93 @@ import FirebaseAuth
 import FirebaseFirestore
 
 struct SignInView: View {
-  @EnvironmentObject var session: SessionStore
-  
-  @State var showPass: Bool = false
-  @State var signUp: Bool = false
-
-  @State var email = "gwells9@gatech.edu"
-  @State var password = "password"
-  @State var confirmPassword = ""
-  @State var successLogin: Bool = false
-  @State var showInformationForm: Bool = false
-  @State var showingAlert = false
-  @State var errorMessage = ""
-  
-  var body: some View {
-    NavigationView {
-      VStack {
+    @EnvironmentObject var session: SessionStore
     
-        VStack(alignment: .leading) {
-            TextField("Email:", text: self.$email)
-                  .padding()
-                  .overlay(RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color(.systemGray3), lineWidth: 1)
-                                      .foregroundColor(.clear))
-
-          // Use a securefield for sensitive info
-          // replaces text with dots and other secure features
-            HStack {
-                if !showPass {
-                    SecureField("Password:", text: self.$password)
+    @State var showPass: Bool = false
+    @State var signUp: Bool = false
+    
+    @State var email = "gwells9@gatech.edu"
+    @State var password = "password"
+    @State var confirmPassword = ""
+    @State var successLogin: Bool = false
+    @State var showInformationForm: Bool = false
+    @State var showingAlert = false
+    @State var errorMessage = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                VStack(alignment: .leading) {
+                    TextField("Email:", text: self.$email)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
                         .padding()
                         .overlay(RoundedRectangle(cornerRadius: 12)
-                                            .stroke(Color(.systemGray3), lineWidth: 1)
-                                            .foregroundColor(.clear))
-                } else {
-                    TextField("Password:", text: self.$password)
+                                    .stroke(Color(.systemGray3), lineWidth: 1)
+                                    .foregroundColor(.clear))
+                    
+                    // Use a securefield for sensitive info
+                    // replaces text with dots and other secure features
+                    SecureInputView("Password:", text: self.$password)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
                         .padding()
                         .overlay(RoundedRectangle(cornerRadius: 12)
-                                            .stroke(Color(.systemGray3), lineWidth: 1)
-                                            .foregroundColor(.clear))
+                                    .stroke(Color(.systemGray3), lineWidth: 1)
+                                    .foregroundColor(.clear))
+                        .padding(.top)
+                    
                 }
+                .padding()
+                
                 Button(action: {
-                                showPass.toggle()
-                            }, label: {
-                                Image(systemName: showPass ? "eye.fill" : "eye.slash.fill")
-                            })
+                    session.signIn(email: self.email, password: self.password) { (signInSuccessful, error) in
+                        if let error = error {
+                            self.errorMessage = error
+                            self.showingAlert = true
+                        }
+                        if signInSuccessful {
+                            self.successLogin = true
+                        }
+                    }
+                }, label: {
+                    Text("Log In")
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 50)
+                        .background(.blue)
+                        .cornerRadius(15)
+                }).padding()
+                // .padding()
+                
+                NavigationLink(destination: SignUpView(), isActive: $signUp) {
+                    Button(action: {
+                        signUp = true
+                    }, label: {
+                        Text("Sign Up")
+                            .frame(width: 100, height: 50)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color(.systemGray3), lineWidth: 1)
+                            )
+                    })
+                } // .padding()
+                Spacer()
+                
             }
-                .padding(.top)
-            
-        }
+            .navigationTitle("Enter GT Study Buddy")
+            .alert(isPresented: self.$showingAlert) {
+                Alert(title: Text("Error"), message: Text(self.errorMessage), dismissButton: .default(Text("Try Again")))
+            }
             .padding()
+        }
         
-        Button(action: {
-          session.signIn(email: self.email, password: self.password) { (signInSuccessful, error) in
-            if let error = error {
-              self.errorMessage = error
-              self.showingAlert = true
-            }
-            if signInSuccessful {
-              self.successLogin = true
-            }
-          }
-        }, label: {
-          Text("Log In")
-            .foregroundColor(.white)
-            .frame(width: 100, height: 50)
-            .background(.blue)
-            .cornerRadius(15)
-        }).padding()
-        // .padding()
-        
-          NavigationLink(destination: SignUpView(), isActive: $signUp) {
-              Button(action: {
-                  signUp = true
-              }, label: {
-                  Text("Sign Up")
-                      .frame(width: 100, height: 50)
-                      .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color(.systemGray3), lineWidth: 1)
-                      )
-              })
-          } // .padding()
-        Spacer()
-        
-      }
-      .navigationTitle("Enter GT Study Buddy")
-      .alert(isPresented: self.$showingAlert) {
-        Alert(title: Text("Error"), message: Text(self.errorMessage), dismissButton: .default(Text("Try Again")))
-      }
-      .padding()
     }
-    
-  }
 }
-    
+
 struct SignInView_Previews: PreviewProvider {
-  static var previews: some View {
-    SignInView()
-      .environmentObject(SessionStore())
-  }
+    static var previews: some View {
+        SignInView()
+            .environmentObject(SessionStore())
+    }
 }
